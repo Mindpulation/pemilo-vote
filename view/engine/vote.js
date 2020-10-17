40526fade572f1fa953d7e9e2c46116064e35dfe
Reza Andriansyah <technoboth@gmail.com>
2020-10-17 10:16:56 +0700

    add schema check in engine techno

--- view/engine/vote.js
+++ view/engine/vote.js
@@ -1,11 +1,15 @@
+const { checkSchemaSave } = require('../../validator/index');
 const { saveVote } = require('../../controller/mongo');
 
 const { Route } = require('wrap_socket');
 const app = new Route();
 
 app.add('sendVote', async (io, eng, param)=>{
-  await saveVote(param);
-  eng.to(param.room).emit('getVote', {idCandidate:param.idCandidate, date:param.date});
+  if(checkSchemaSave(param)){
+    await saveVote(param);
+    eng.to(param.room).emit('getVote', {idCandidate:param.idCandidate, date:param.date});
+  }
+  else{console.log("Format Salah");}
 });
 
 module.exports = app.ex();
